ca214f2c437a8cfdf8ae79951210f6e3f7f10f2c
sona-R <sonaramadas123@gmail.com>
2024-02-19 22:41:21 +0530

    first commit

--- task2/whitespace.dart
+++ task2/whitespace.dart
@@ -1,5 +1,5 @@
 void main() {
-  String inputString = "hi iam ashique nice to see you iam waiting";
+  String inputString = "hi iam sona nice to see you iam waiting";
 
   String stringWithoutSpaces = inputString.replaceAll(RegExp(r'\s+'), '');
 
